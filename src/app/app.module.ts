e712b147dbf222a2f7d553bfc5f0c0af88fc83c9
mansourlotfi <mansourlotfi@gmail.com>
2020-01-22 23:31:09 +0330

    hader component manually added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
 @NgModule({
-	declarations: [ AppComponent ],
+	declarations: [ AppComponent, HeaderComponent, RecipesComponent ],
 	imports: [ BrowserModule ],
 	providers: [],
 	bootstrap: [ AppComponent ]
