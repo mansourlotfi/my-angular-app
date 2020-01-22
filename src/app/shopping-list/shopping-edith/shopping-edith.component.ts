01f97bb3add75fb3e16d5d0f96613d013bbb6b3c
mansourlotfi <mansourlotfi@gmail.com>
2020-01-22 23:44:23 +0330

    adding component via cli command : ng g c <componentName> --spec false

---
+++ src/app/shopping-list/shopping-edith/shopping-edith.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-edith',
+  templateUrl: './shopping-edith.component.html',
+  styleUrls: ['./shopping-edith.component.css']
+})
+export class ShoppingEdithComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
